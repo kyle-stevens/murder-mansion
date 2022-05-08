56ffdd795356693cada2528d66fa6d7a58ad31c0
Kyle Stevens <43458322+kyle-stevens@users.noreply.github.com>
2022-05-08 13:18:21 -0700

    animation checks and display with motion tracking objects - v0.2.1

--- murder-mansion - rework/game/Scenes/ybot_animated_test.gd
+++ murder-mansion - rework/game/Scenes/ybot_animated_test.gd
@@ -4,13 +4,39 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+var counter = 0
+var animations
+var animation_index = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animations = [
+		"animationIdle",
+		"animationLeftStrafeRun",
+		"animationLeftStrafeWalk",
+		"animationRightStrafeRun",
+		"animationRightStrafeWalk",
+		"animationRun",
+		"animationWalk"
+	]
+	print(animations)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$AnimationPlayer.play("animationIdle")
+	
+	
+	counter = counter + 1
+	#print(counter % 100)
+	#print(animation_index)
+	#print(animations[animation_index])
+	$AnimationPlayer.play(animations[animation_index])
+	if (counter > 100):
+		counter = 0
+		animation_index += 1
+		print(animation_index)
+		print("ITERATE ANIM")
+		if animation_index >= 7:
+			print("RESET ANIM")
+			animation_index = 0
+	
+	#self.transform.origin = self.transform.origin + Vector3(0,0,0.01)
