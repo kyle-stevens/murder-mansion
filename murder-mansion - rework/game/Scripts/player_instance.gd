9c3db29e6d039586d0078dac96d35e4da472f6ef
Kyle Stevens <43458322+kyle-stevens@users.noreply.github.com>
2022-05-13 17:28:41 -0700

    V0.2.3 - Patch 3
    -Networking figured out to an extent
    -Issue with syncing player model choice across connection
    -Patch 4 to include hats

--- murder-mansion - rework/game/Scripts/player_instance.gd
+++ murder-mansion - rework/game/Scripts/player_instance.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+var player_name = ""
+
 ###CONSTANTS###################################################################
 const ACCEL = 4.5
 const DEACCEL = 16
@@ -7,19 +9,17 @@ const SPRINT_ACCEL = 18
 const MAX_SLOPE_ANGLE = 40
 const MAX_SPEED = 8
 const MAX_SPRINT_SPEED = 15
-
 const JUMP_SPEED = 9
 const GRAVITY = -24.8
 
-var player_name = ""
-
 ###PUPPET VARIABLES############################################################
-var puppet_name : String
-var puppet_pos : Vector3
-var puppet_vel : Vector3
-var puppet_rot : Vector3
-var puppet_flash : bool
-var puppet_anim : String
+var puppet_name : String = ""
+var puppet_pos : Vector3 = Vector3()
+var puppet_vel : Vector3 = Vector3()
+var puppet_rot : Vector2 = Vector2()
+var puppet_flash : bool = true
+var puppet_anim : String = "animationIdle"
+var puppet_model : String = "Male"
 export(NodePath) onready var movement_tween = get_node("movement_tween") as Tween
 export(NodePath) onready var network_tick_rate = get_node("network_tick_rate") as Timer
 
@@ -29,13 +29,13 @@ var current_animation : String
 
 ###MOVEMENT####################################################################
 var is_sprinting : bool
-var vel : Vector3
-var dir : Vector3
+var vel : Vector3 = Vector3()
+var dir : Vector3 = Vector3()
 var sprint_energy : int
 var sprint_recharge : bool
 
 ###FLASHLIGHT##################################################################
-var flashlight : Spatial
+var flashlight : SpotLight
 
 ###CAMERA######################################################################
 onready var camera = get_node("rotation_helper/player_camera")
@@ -54,40 +54,48 @@ onready var player_model = get_node("player_model")
 func _ready():
 	#Check if Instance is Network Master for Camera Focus
 	camera.current = is_network_master()
-	print(PlayerVariables.player_model)
+
 	#Set Model for Player
-	if PlayerVariables.player_model == "Male":
-		print("Male")
-		var instance = load("res://Scenes/ybot.tscn").instance()
-		instance.set_name("player_model")
-		player_model.add_child(instance)
-	elif PlayerVariables.player_model == "Female":
-		print("Female")
-		var instance = load("res://Scenes/xbot.tscn").instance()
-		instance.set_name("player_model")
-		player_model.add_child(instance)
-	
+	if is_network_master():
+		if PlayerVariables.player_model == "Male":
+			var instance = load("res://Scenes/ybot.tscn").instance()
+			instance.set_name("player_model")
+			player_model.add_child(instance)
+		elif PlayerVariables.player_model == "Female":
+			var instance = load("res://Scenes/xbot.tscn").instance()
+			instance.set_name("player_model")
+			player_model.add_child(instance)
+	else:
+		if puppet_model == "Male":
+			var instance = load("res://Scenes/ybot.tscn").instance()
+			instance.set_name("player_model")
+			player_model.add_child(instance)
+		elif puppet_model == "Female":
+			var instance = load("res://Scenes/xbot.tscn").instance()
+			instance.set_name("player_model")
+			player_model.add_child(instance)
+
 	#Getting Flashlight Node from player_model child
 	flashlight = get_node("player_model/player_model/Skeleton/"+
 		"FlashlightAttachment/flashlight/SpotLight")
-	
+
 	#Getting Animation Node from player_model child
 	animation_player = get_node("player_model/player_model/AnimationPlayer")
-	
+
 	#Set Initial Animation
 	animation_player.play("animationIdle")
-	
+
 	#Initial Mouse Mode
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
+
 	#Set Sprint Energy
 	sprint_energy = 100
 	sprint_recharge = false
 
-func _process(delta):
+func _physics_process(delta):
 	process_inputs(delta)
 	process_movement(delta)
-	
+
 func _input(event):
 	if is_network_master():
 		if event is InputEventMouseMotion and \
@@ -96,16 +104,16 @@ func _input(event):
 					deg2rad(event.relative.y*mouse_sensitivity * 1))
 					#change to 1 for inverted mouse up/dwn
 			self.rotate_y(
-					deg2rad(event.relative.x*mouse_sensitivity * -1)) 
+					deg2rad(event.relative.x*mouse_sensitivity * -1))
 					#change to 1 for inverted mouse left/right
-			
+
 			var camera_rot = rotation_helper.rotation_degrees
 			camera_rot.x = clamp(camera_rot.x, -x_range, x_range)
-			
+
 			rotation_helper.rotation_degrees = camera_rot
 
 func process_inputs(delta):
-		print(sprint_energy)
+		#make sprint energy a bar
 		if is_network_master():
 			#Handling Sprint Recharge Mechanic
 			if sprint_recharge:
@@ -119,27 +127,27 @@ func process_inputs(delta):
 					sprint_recharge = true
 			if sprint_energy > 100:
 				sprint_energy = 100
-				
+
 			#Purely for demo instancing of player
 			#print(vel)
-			
+
 			#Check if Jumping
 			if is_on_floor(): #keeps motion while in jump
-				dir = Vector3() 
+				dir = Vector3()
 
 			#Camera Transform
 			var cam_xform = camera.get_global_transform()
-			
+
 			#Set Input Movement
 			var input_movement_vector = Vector2()
-			
+
 			#Booleans for Movement
 			var forward = false
 			var backward = false
 			var left = false
 			var right = false
-			
-			
+
+
 			if Input.is_action_pressed("movement_forward"):
 				input_movement_vector.y += 1
 				forward = true
@@ -152,7 +160,7 @@ func process_inputs(delta):
 			if Input.is_action_pressed("movement_right"):
 				input_movement_vector.x += 1
 				right = true
-			
+
 			if not is_sprinting:
 				sprint_energy += 1
 				if forward and is_on_floor():
@@ -160,14 +168,14 @@ func process_inputs(delta):
 						animation_player.play("animationLeftStrafeWalk")
 					elif right:
 						animation_player.play("animationRightStrafeWalk")
-					else:	
+					else:
 						animation_player.play("animationWalk")
 				elif backward and is_on_floor():
 					if left:
 						animation_player.play_backwards("animationRightStrafeWalk")
 					elif right:
 						animation_player.play_backwards("animationLeftStrafeWalk")
-					else:	
+					else:
 						animation_player.play_backwards("animationWalk")
 				elif left and is_on_floor():
 					animation_player.play("animationLeftStrafeWalk")
@@ -180,37 +188,37 @@ func process_inputs(delta):
 						animation_player.play("animationLeftStrafeRun")
 					elif right:
 						animation_player.play("animationRightStrafeRun")
-					else:	
+					else:
 						animation_player.play("animationRun")
 				elif backward and is_on_floor():
 					if left:
 						animation_player.play_backwards("animationRightStrafeRun")
 					elif right:
 						animation_player.play_backwards("animationLeftStrafeRun")
-					else:	
+					else:
 						animation_player.play_backwards("animationRun")
 				elif left and is_on_floor():
 					animation_player.play("animationLeftStrafeRun")
 				elif right and is_on_floor():
 					animation_player.play("animationRightStrafeRun")
-			
-			
-			
+
+
+
 			if abs(vel.x) < 1 and abs(vel.y) < 1 and abs(vel.z) < 1 and is_on_floor():
 				animation_player.play("animationIdle")
 			if not is_on_floor():
 				animation_player.play("animationFalling")
 			input_movement_vector = input_movement_vector.normalized()
-			
+
 			dir += -cam_xform.basis.z * input_movement_vector.y
 			dir += cam_xform.basis.x * input_movement_vector.x
-			
+
 			#Jump Movement
 			if is_on_floor():
 				if Input.is_action_just_pressed("movement_jump"):
 					vel.y = JUMP_SPEED
 					animation_player.play("animationFalling")
-			
+
 			#Cursor Freeing
 			if Input.is_action_just_pressed("ui_cancel"):
 				print("cursor freeing")
@@ -218,14 +226,14 @@ func process_inputs(delta):
 					Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 				else:
 					Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			
+
 			#Flashlight Toggle
 			if Input.is_action_just_pressed("player_flashlight"):
 				if flashlight.visible == true:
 					flashlight.visible = false
 				else:
 					flashlight.visible = true
-			
+
 			#Sprinting Movement
 			if Input.is_action_pressed("movement_sprint"):
 				is_sprinting = true
@@ -234,7 +242,7 @@ func process_inputs(delta):
 
 func process_movement(delta):
 	dir.y = 0
-	dir = dir.normalized()	
+	dir = dir.normalized()
 	vel.y += delta * GRAVITY
 	if is_network_master():
 		var hvel = vel
@@ -264,72 +272,50 @@ func process_movement(delta):
 		rotation.y = puppet_rot.y
 		camera.rotation.x = puppet_rot.x
 		flashlight.visible = puppet_flash
-	
-		
-	
+
+
+
 	if !movement_tween.is_active():
 		vel = move_and_slide(
-				vel, 
-				Vector3(0,1,0), 
-				0.05, 
-				4, 
-				deg2rad(MAX_SLOPE_ANGLE)	
+				vel,
+				Vector3(0,1,0),
+				0.05,
+				4,
+				deg2rad(MAX_SLOPE_ANGLE)
 				)
 
 remote func _set_position(pos):
 	global_transform.origin = pos
 
-func _on_Area_body_entered(body):
-	#Highlight RigidBody
-	if body is RigidBody:
-		var CSGSPHERE : Node = body.get_node("CSGSphere")
-		#CSGSPHERE.material_override = \
-		#load("res://Shaders_and_Materials/plain_white_material_outline.tres")
-		
-		#print(CSGSPHERE)
-		#reticle.color = Color(0,1,0,1)
-	pass
-
-func _on_Area_body_exited(body):
-	#Remove Highlight on RigidBody
-	if body is RigidBody:
-		var CSGSPHERE : Node = body.get_node("CSGSphere")
-		#CSGSPHERE.material_override = \
-		#load("res://Shaders_and_Materials/plain_white_material.tres")
-		
-		#print(CSGSPHERE)
-		#reticle.color = Color(1,1,1,1)
-	pass
-	pass # Replace with function body.
-
-func _on_damage_area_body_entered(body):
-	if body is RigidBody and body.damage != 0 and body.thrower != self:
-		#self.reticle.color = Color(1,0,0,1)
-		print("Damaged")
-		animation_player.play("death")
-		$collision_body.queue_free()
-		$collision_feet.queue_free()
-		$rotation_helper.queue_free()
-		$player_hud.queue_free()
-		$damage_area.queue_free()
-		get_tree()
-		#dead = true
-		#queue_free() or spectator mode
-		#Make Ghost instance
-		body.queue_free()
-
-puppet func update_state(p_position, p_velocity, p_rotation, p_flashlight_on, p_animation, p_player_name, held_object_name, held_object_position):
+puppet func update_state(p_position, p_velocity, p_rotation, p_flashlight_on, p_animation, p_player_name, p_player_model, held_object_position):
 	puppet_pos = p_position
 	puppet_rot = p_rotation
 	puppet_vel = p_velocity
 	puppet_flash = p_flashlight_on
 	puppet_anim = p_animation
 	puppet_name = p_player_name #player username
+	puppet_model = p_player_model
 	movement_tween.interpolate_property(self, "global_transform", global_transform, Transform(global_transform.basis, p_position), 0.1)
 	movement_tween.start()
-
-func _on_NetworkTickRate_timeout():
+	
+	#Check for host model
+	if puppet_model != PlayerVariables.player_model:
+		player_model.get_node("player_model").queue_free()
+		####HERE IS THE RESET!!
+		if puppet_model == "Male":
+			var instance = load("res://Scenes/ybot.tscn").instance()
+			instance.set_name("player_model")
+			player_model.add_child(instance)
+		elif puppet_model == "Female":
+			var instance = load("res://Scenes/xbot.tscn").instance()
+			instance.set_name("player_model")
+			player_model.add_child(instance)
+	
+func _on_network_tick_rate_timeout():
+	print("timeout")
 	if is_network_master():
-		rpc_unreliable("update_state", global_transform.origin, vel, Vector2(camera.rotation.x, rotation.y), flashlight.visible, animation_player.current_animation, PlayerVariables.player_name, "none", "none")
+		print("timeout")
+		rpc_unreliable("update_state", global_transform.origin, vel, Vector2(camera.rotation.x, rotation.y), flashlight.visible, animation_player.current_animation, PlayerVariables.player_name, PlayerVariables.player_model, "none")
 	else:
 		network_tick_rate.stop()
+
--- murder-mansion - rework/game/Scripts/player_instance_refactor.gd
+++ murder-mansion - rework/game/Scripts/player_instance_refactor.gd
@@ -2,108 +2,87 @@ extends KinematicBody
 
 var player_name = ""
 
-#To Do List
-#->Adding in Damage and Demo Networking
-#->Code Refactoring
+###CONSTANTS###################################################################
+const ACCEL = 4.5
+const DEACCEL = 16
+const MAX_SLOPE_ANGLE = 40
+const MAX_SPEED = 8#15
+const MAX_SPRINT_SPEED = 20
+const SPRINT_ACCEL = 18
+const JUMP_SPEED = 9
+const GRAVITY = -24.8
 
+###PUPPET VARIABLES############################################################
 var puppet_position = Vector3()
 var puppet_velocity = Vector3()
 var puppet_rotation = Vector3()
 var puppet_flashlight = true
 var puppet_animation = "default"
 
-var object_name = "none"
-var object_transform
 
+###NETWORKING##################################################################
+export(NodePath) onready var network_tick_rate = get_node("network_tick_rate") as Timer
+export(NodePath) onready var movement_tween = get_node("movement_tween") as Tween
 
-#Animation
-#set up some sort of switch for this
-#onready var animPlayer = get_node("player_model/femaleModel/AnimationPlayer")
-onready var animPlayer = get_node("player_model/maleModel/AnimationPlayer")
-#getting the fps-ness back
-export(NodePath) onready var model = get_node(model) as Spatial
-export(NodePath) onready var head = get_node(head) as Spatial
-export(NodePath) onready var camera = get_node(camera) as Camera
-export(NodePath) onready var network_tick_rate = get_node(network_tick_rate) as Timer
-export(NodePath) onready var movement_tween = get_node(movement_tween) as Tween
+var dead = false #deprecated for now
 
-var dead = false
-var killer = true
-
-#Basic Movement
-const ACCEL = 4.5
-const DEACCEL = 16
-const MAX_SLOPE_ANGLE = 40
-const MAX_SPEED = 8#15
+###MOVEMENT####################################################################
 var vel = Vector3()
 var dir = Vector3()
-
-#Jump Movement
-const JUMP_SPEED = 9
-var gravity = -24.8
-
-#Sprint Movement
 var is_sprinting = false
-var MAX_SPRINT_SPEED = 20
-var SPRINT_ACCEL = 18
 
-#Flashlight
+###FLASHLIGHT##################################################################
 var flashlight
 
-#Camera
+###CAMERA######################################################################
 export var x_range = 70
+onready var camera = get_node("rotation_helper/player_camera") as Camera
+onready var head = get_node("rotation_helper")
 
-#Grabbing RigidBodys
-var grabbed_object = null
-export var OBJECT_THROW_FORCE = 2#120 #change to const upon balancing
-export var OBJECT_GRAB_DISTANCE = 7 #change to const upon balancing
-export var OBJECT_GRAB_RAY_DISTANCE = 10 #change to const upon balancing
-var object_detection
-var object_detection_collision
-var objectToThrowVar
-
-#Rotation Helper
+###ROTATION HELPER#############################################################
 var rotation_helper
 export var MOUSE_SENSITIVITY = 0.05
 
-#UI
+###UI##########################################################################
 var reticle
 
+###ANIMATION###################################################################
+var model
+var animation_player
 
-func _ready():	
-	
+func _ready():
+
 	camera.current = is_network_master()
-	
+
 	#Rotation Helper
 	rotation_helper = $rotation_helper
-	
-	#Flashlight
-	flashlight = $rotation_helper/player_flashlight
-	
+
 	#UI
-	reticle = $player_hud/Reticle
-	reticle.set_position(get_viewport().size / 2)
+	#reticle = $player_hud/Reticle
+	#reticle.set_position(get_viewport().size / 2)
 	#Change Var for Color Shift
-	reticle = $player_hud/Reticle/ColorRect
-	
-	#Grabbing Objects
-	object_detection = $rotation_helper/gun_fire_points
-	object_detection_collision = \
-			$rotation_helper/gun_fire_points/grab_objects/Area/CollisionShape
-			
-	#Set initial Animation
-	animPlayer.play("default")
-	
-	if true:
-		#Set Area to Grab Distance
-		object_detection.translation = \
-				Vector3(0,0,OBJECT_GRAB_RAY_DISTANCE / 2 * -1)
-		object_detection_collision.get_shape().set_extents(
-				Vector3(.05,.05,OBJECT_GRAB_RAY_DISTANCE / 2)
-				)
-				
+	#reticle = $player_hud/Reticle/ColorRect
+
+	#Set Model for Player
+	if PlayerVariables.player_model == "Male":
+		var instance = load("res://Scenes/ybot.tscn").instance()
+		instance.set_name("player_model")
+		player_model.add_child(instance)
+	elif PlayerVariables.player_model == "Female":
+		var instance = load("res://Scenes/xbot.tscn").instance()
+		instance.set_name("player_model")
+		player_model.add_child(instance)
+
+
+	#ANIMATION
+	animation_player = get_node("player_model/player_model/AnimationPlayer")
+	animation_player.play("animationIdle")
+
+	flashlight = get_node("player_model/player_model/Flashlight/SpotLight")
+
 	#Initial Mouse Mode
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 
 func _physics_process(delta):
@@ -112,61 +91,74 @@ func _physics_process(delta):
 			camera.transform.origin = Vector3(0,1.4,0)
 		else:
 			camera.transform.origin = Vector3(0,1.4,4.4)
-		
-	
+
+
 	if dead:
-		gravity = 0
-	
+		#GRAVITY = 0
+		pass
+
 	process_inputs(delta)
 	process_movement(delta)
-	
+
 	if object_name != "none": #change to a check if the value has changed
 		var node_Test = get_tree().get_root().get_node(object_name)
 		#print(node_Test)
-		
+
 
 
 func _input(event):
 	if is_network_master():
 		#Purely for demo instancing of player
 		#print(vel)
-		
+
 		if event is InputEventMouseMotion and \
 		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			rotation_helper.rotate_x(
 					deg2rad(event.relative.y*MOUSE_SENSITIVITY * -1))
 					#change to 1 for inverted mouse up/dwn
 			self.rotate_y(
-					deg2rad(event.relative.x*MOUSE_SENSITIVITY * -1)) 
+					deg2rad(event.relative.x*MOUSE_SENSITIVITY * -1))
 					#change to 1 for inverted mouse left/right
-			
+
 			var camera_rot = rotation_helper.rotation_degrees
 			camera_rot.x = clamp(camera_rot.x, -x_range, x_range)
-			
+
 			rotation_helper.rotation_degrees = camera_rot
 
 
 func process_inputs(delta):
 		if is_network_master():
+			#Handling Sprint Recharge Mechanic
+			if sprint_recharge:
+				if sprint_energy <= 50:
+					is_sprinting = false
+				else:
+					sprint_recharge = false
+			else:
+				if sprint_energy <= 0:
+					is_sprinting = false
+					sprint_recharge = true
+			if sprint_energy > 100:
+				sprint_energy = 100
 			#Purely for demo instancing of player
 			#print(vel)
-			
+
 			#Check if Jumping
 			if is_on_floor(): #keeps motion while in jump
-				dir = Vector3() 
+				dir = Vector3()
 
 			#Camera Transform
 			var cam_xform = camera.get_global_transform()
-			
+
 			#Set Input Movement
 			var input_movement_vector = Vector2()
-			
+
 			#Booleans for Movement
 			var forward = false
 			var backward = false
 			var left = false
 			var right = false
-			
+
 			#Basic Movement - only way to avoid animation glitch is elif statements instead of all ifs
 			if Input.is_action_pressed("movement_forward"):
 				input_movement_vector.y += 1
@@ -180,44 +172,44 @@ func process_inputs(delta):
 			if Input.is_action_pressed("movement_right"):
 				input_movement_vector.x += 1
 				right = true
-			
+
 			#handlingAnimations - walk has been exchanged with slow run
 			if forward and is_on_floor():
 				if left:
-					animPlayer.play("leftstrafe")
+					animation_player.play("leftstrafe")
 				elif right:
-					animPlayer.play("rightstrafe")
-				else:	
-					animPlayer.play("walk")
+					animation_player.play("rightstrafe")
+				else:
+					animation_player.play("walk")
 			elif backward and is_on_floor():
 				if left:
-					animPlayer.play_backwards("rightstrafe")
+					animation_player.play_backwards("rightstrafe")
 				elif right:
-					animPlayer.play_backwards("leftstrafe")
-				else:	
-					animPlayer.play_backwards("walk")
+					animation_player.play_backwards("leftstrafe")
+				else:
+					animation_player.play_backwards("walk")
 			elif left and is_on_floor():
-				animPlayer.play("leftstrafe")
+				animation_player.play("leftstrafe")
 			elif right and is_on_floor():
-				animPlayer.play("rightstrafe")
-			
-			
-			
+				animation_player.play("rightstrafe")
+
+
+
 			if abs(vel.x) < 1 and abs(vel.y) < 1 and abs(vel.z) < 1 and is_on_floor():
-				animPlayer.play("default")
+				animation_player.play("default")
 			if not is_on_floor():
-				animPlayer.play("jump")
+				animation_player.play("jump")
 			input_movement_vector = input_movement_vector.normalized()
-			
+
 			dir += -cam_xform.basis.z * input_movement_vector.y
 			dir += cam_xform.basis.x * input_movement_vector.x
-			
+
 			#Jump Movement
 			if is_on_floor():
 				if Input.is_action_just_pressed("movement_jump"):
 					vel.y = JUMP_SPEED
-					animPlayer.play("jump")
-			
+					animation_player.play("jump")
+
 			#Cursor Freeing
 			if Input.is_action_just_pressed("ui_cancel"):
 				print("cursor freeing")
@@ -225,7 +217,7 @@ func process_inputs(delta):
 					Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 				else:
 					Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			
+
 			#Flashlight Toggle
 			if Input.is_action_just_pressed("player_flashlight"):
 				#var flashlight = $rotation_helper/player_flashlight
@@ -233,7 +225,7 @@ func process_inputs(delta):
 					flashlight.visible = false
 				else:
 					flashlight.visible = true
-			
+
 			#Sprinting Movement
 			if Input.is_action_pressed("movement_sprint"):
 				is_sprinting = true
@@ -245,8 +237,8 @@ func process_inputs(delta):
 
 func process_movement(delta):
 	dir.y = 0
-	dir = dir.normalized()	
-	vel.y += delta * gravity
+	dir = dir.normalized()
+	vel.y += delta * GRAVITY
 	if is_network_master():
 		var hvel = vel
 		hvel.y = 0
@@ -269,38 +261,26 @@ func process_movement(delta):
 		vel.z = hvel.z
 	else:
 		global_transform.origin = puppet_position
-		animPlayer.play(puppet_animation)
+		animation_player.play(puppet_animation)
 		vel.x = puppet_velocity.x
 		vel.z = puppet_velocity.z
 		rotation.y = puppet_rotation.y
 		head.rotation.x = puppet_rotation.x
 		flashlight.visible = puppet_flashlight
-	
-		
-	
+
+
+
 	if !movement_tween.is_active():
 		vel = move_and_slide(
-				vel, 
-				Vector3(0,1,0), 
-				0.05, 
-				4, 
-				deg2rad(MAX_SLOPE_ANGLE)	
+				vel,
+				Vector3(0,1,0),
+				0.05,
+				4,
+				deg2rad(MAX_SLOPE_ANGLE)
 				)
 
 remote func _set_position(pos):
 	global_transform.origin = pos
-
-#Considering Highlighting Objects or Highlighting/Changing HUD Reticle
-func _on_Area_body_entered(body):
-	#Highlight RigidBody
-	if body is RigidBody:
-		var CSGSPHERE : Node = body.get_node("CSGSphere")
-		#CSGSPHERE.material_override = \
-		#load("res://Shaders_and_Materials/plain_white_material_outline.tres")
-		
-		#print(CSGSPHERE)
-		#reticle.color = Color(0,1,0,1)
-	pass
 
 
 func _on_Area_body_exited(body):
@@ -309,7 +289,7 @@ func _on_Area_body_exited(body):
 		var CSGSPHERE : Node = body.get_node("CSGSphere")
 		#CSGSPHERE.material_override = \
 		#load("res://Shaders_and_Materials/plain_white_material.tres")
-		
+
 		#print(CSGSPHERE)
 		#reticle.color = Color(1,1,1,1)
 	pass
@@ -320,7 +300,7 @@ func _on_damage_area_body_entered(body):
 	if body is RigidBody and body.damage != 0 and body.thrower != self:
 		#self.reticle.color = Color(1,0,0,1)
 		print("Damaged")
-		animPlayer.play("death")
+		animation_player.play("death")
 		$collision_body.queue_free()
 		$collision_feet.queue_free()
 		$rotation_helper.queue_free()
@@ -332,8 +312,8 @@ func _on_damage_area_body_entered(body):
 		#Make Ghost instance
 		body.queue_free()
 		#implement damage here
-		
-	
+
+
 puppet func update_state(p_position, p_velocity, p_rotation, p_flashlight_on, p_animation, p_player_name, held_object_name, held_object_position):
 	puppet_position = p_position
 	puppet_rotation = p_rotation
@@ -351,10 +331,9 @@ puppet func update_state(p_position, p_velocity, p_rotation, p_flashlight_on, p_
 
 func _on_NetworkTickRate_timeout():
 	if is_network_master():
-		print("timeout")
 		if grabbed_object != null:
-			rpc_unreliable("update_state", global_transform.origin, vel, Vector2(head.rotation.x, rotation.y), flashlight.visible, animPlayer.current_animation, player_name, grabbed_object.name, grabbed_object.global_transform.origin)
+			rpc_unreliable("update_state", global_transform.origin, vel, Vector2(head.rotation.x, rotation.y), flashlight.visible, animation_player.current_animation, player_name, grabbed_object.name, grabbed_object.global_transform.origin)
 		else:
-			rpc_unreliable("update_state", global_transform.origin, vel, Vector2(head.rotation.x, rotation.y), flashlight.visible, animPlayer.current_animation, player_name, "none", "none")
+			rpc_unreliable("update_state", global_transform.origin, vel, Vector2(head.rotation.x, rotation.y), flashlight.visible, animation_player.current_animation, player_name, "none", "none")
 	else:
 		network_tick_rate.stop()
