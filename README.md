0ffd68136ed21968bfe6da7ad4ae7173eb1b5fbd
kyle-stevens <43458322+kyle-stevens@users.noreply.github.com>
2022-05-23 20:34:36 +0000

    Deploying to main from  @ a920fd1c8425ba78f32916e6d607794697649a00 🚀

--- README.md
+++
@@ -1,2 +0,0 @@
-# murder-mansion
-A 3d game built using open-source game enginge Godot to create a third person Clue/Werewolves style murder mystery game. 
--- source_files/murder-mansion - rework/game/Scripts/player_variables.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var player_model : String = "Female"
-var player_name : String = "Victim"
-var player_color : String = "res://Assets/player_materials/Red.tres"
-var player_hat : String = ""
-var player_active_camera : Camera
-
