a81aeaf611f8180d0781ed5f133a75b700ff80b9
Kyle Stevens <43458322+kyle-stevens@users.noreply.github.com>
2022-06-16 14:29:24 -0700

    v0.2.9 - NetworkSetup Refactoring 1/1
    -Added Comments and removed unnecessary comments and whitespace
    -Standardized Format

--- source_files/murder-mansion - rework/game/Scripts/NetworkSetup.gd
+++ source_files/murder-mansion - rework/game/Scripts/NetworkSetup.gd
@@ -1,36 +1,53 @@
+###############################################################################
+# Network Setup Script : ######################################################
+###############################################################################
+
 extends Control
 
+###NETWORK SETUP VARIABLES#####################################################
 var _player_name = "Player"
 
+###############################################################################
+# Built-in Ready Function when Network Setup is called ########################
+###############################################################################
 func _ready():
 	Global.connect("toggle_network_setup",self,"_toggle_network_setup")
 
-func _on_IpAddress_text_changed(new_text):
-	Network.ip_address = new_text
-
-
+###############################################################################
+# Handling Host button 'pressed' signal #######################################
+###############################################################################
 func _on_Host_pressed():
-	
-#	hide()
-#	Global.emit_signal("instance_player", get_tree().get_network_unique_id())
-#Still trying to figure out dedicated server stuff
+    # If the Player Name is 'dedicated_server'. This is likely to be deprecated
+    # and removed at a later date
 	if _player_name == "dedicated_server":
 		Network.create_server()
-		print("DEDICATED SERVER")
-		hide()
+        # hide the ui console menu
+        hide()
+        # Do not instance a player for the dedicated server
 	else:
 		Network.create_server()
-		hide()
-		Global.emit_signal("instance_player", get_tree().get_network_unique_id()) #setting up only dedicated server system
+        # hide the ui console menu
+        hide()
+		Global.emit_signal("instance_player",
+            get_tree().get_network_unique_id())
 
+###############################################################################
+# Handling Join button 'pressed' signal #######################################
+###############################################################################
 func _on_Join_pressed():
 	Network.join_server()
 	hide()
 	Global.emit_signal("instance_player", get_tree().get_network_unique_id())
 
+###############################################################################
+# Handling the network setup menu toggle ######################################
+###############################################################################
 func _toggle_network_setup(visible_toggle):
 	visible = visible_toggle
-	
+
+###############################################################################
+# Monitor the player name for in game nickname ################################
+###############################################################################
 func _on_PlayerName_text_changed(new_text):
 	_player_name = new_text
 	#print(_player_name)
