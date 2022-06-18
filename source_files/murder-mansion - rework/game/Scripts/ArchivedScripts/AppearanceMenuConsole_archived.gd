0b78643f32223afa9d3dad29ec6f0224ba9e2490
Kyle Stevens <43458322+kyle-stevens@users.noreply.github.com>
2022-06-18 15:12:28 -0700

    v0.2.9 - AppearanceMenuConsole Refactoring 1/1
    -Added Comments and reformatted

--- source_files/murder-mansion - rework/game/Scripts/ArchivedScripts/AppearanceMenuConsole_archived.gd
+++ source_files/murder-mansion - rework/game/Scripts/ArchivedScripts/AppearanceMenuConsole_archived.gd
@@ -1,32 +1,32 @@
-###############################################################################
-# AppearanceMenuConsole Script ################################################
-###############################################################################
-
 extends Area
 
-###VARIABLES###################################################################
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 var at_console = false
 
-# Ready Function - Does Nothing for MenuConsole
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
 
-# Maintain Checks for if the player is near the console to interact with it
+
+
 func _process(delta):
 	if at_console and Input.is_action_just_pressed("interact"):
 		add_child(load("res://Scenes/AppearanceMenu.tscn").instance())
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-# Change the at_console variable to 'true' if the player model has entered the
-# detection area
+
 func _on_AppearanceMenuConsole_body_entered(body):
 	if body is KinematicBody and body.is_network_master():
 		at_console = true
 		print(at_console)
 		body.get_node("UI/popupMesg").text = "press 'E' to interact"
 
-# Change the at_console variable to 'false' if the player model has entered the
-# detection area
+
+
+
 func _on_AppearanceMenuConsole_body_exited(body):
 	if body is KinematicBody and body.is_network_master():
 		at_console = false
