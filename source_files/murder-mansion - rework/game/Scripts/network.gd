356bcd0e17c1a74c56c8d5a0fe77170bb8aa6b17
Kyle Stevens <43458322+kyle-stevens@users.noreply.github.com>
2022-06-19 13:43:53 -0700

    v0.2.9 - Network Refactoring 1/2
    -Added comments
    -Reformatted code

--- source_files/murder-mansion - rework/game/Scripts/network.gd
+++ source_files/murder-mansion - rework/game/Scripts/network.gd
@@ -1,19 +1,36 @@
+###############################################################################
+# Network Script ##############################################################
+###############################################################################
+
 extends Node
+
+###NETWORK CONSTANTS###########################################################
 
 const DEFAULT_PORT= 28960
 const MAX_CLIENTS = 5
 
+###NETWORK VARIABLES###########################################################
+
 var server = null
 var client = null
 
+# Default IP Address
 var ip_address = "127.0.0.1"
 
+###############################################################################
+# Ready Function - Preps basic connections for network functionality and ######
+# Network Functions ###########################################################
+###############################################################################
 func _ready():
 	get_tree().connect("connected_to_server",self,"_connected_to_server")
 	get_tree().connect("server_disconnected",self,"_server_disconnected")
 	get_tree().connect("connection_failed",self,"_connection_failed")
 	get_tree().connect("network_peer_connected",self,"_player_connected")
 
+###############################################################################
+# Create Server Function - Implements UPNP when applicable, otherwise creates #
+# a server at home IP address #################################################
+###############################################################################
 func create_server():
 	#upnp cannot detect a router .... this is an issue
 	var upnp = UPNP.new()
@@ -32,6 +49,10 @@ func create_server():
 	get_tree().set_network_peer(server)
 	upnp.delete_port_mapping(28960)
 	
+###############################################################################
+# Join Server Function - Implements instancing of new player and adds network #
+# peer to the current server ##################################################
+###############################################################################
 func join_server():
 	print("JOINING SERVER..." + ip_address)
 	
@@ -39,20 +60,35 @@ func join_server():
 	client.create_client(ip_address,DEFAULT_PORT)
 	get_tree().set_network_peer(client)
 
+###############################################################################
+
+###############################################################################
 func _connected_to_server():
 	print("CONNECTED SUCCESSFULLY")
-	
+
+###############################################################################
+
+###############################################################################
 func _server_disconnected():
 	print("DISCONNECTED FROM SERVER")
 	reset_network_connection()
-	
+
+###############################################################################
+
+###############################################################################
 func _connection_failed():
 	print("CONNECTION ATTEMPT FAILED")
 	reset_network_connection()
-	
+
+###############################################################################
+
+###############################################################################
 func reset_network_connection():
 	if get_tree().has_network_peer():
 		get_tree().network_peer = null
 
+###############################################################################
+
+###############################################################################
 func _player_connected(id):
 	print("PEER " + str(id) + " HAS CONNECTED")
